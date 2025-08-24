5f9279422975c90a6617977e0045d0f4edcd835b
Ivan Kramarenko <kramarenko-ivan@proton.me>
2025-08-24 09:35:19 +0300

    feat(tests): add test for retrieving tracks for a user

--- backend/app/tests/test_track.py
+++ backend/app/tests/test_track.py
@@ -52,3 +52,21 @@ def test_get_tracks_for_user(
 
     assert habit1["id"] in habit_ids
     assert habit2["id"] in habit_ids
+
+
+def test_get_tracks_for_habit(
+    test_user: Mapping[str, str],
+    habit_factory: Callable[[int, str], Dict[str, str]],
+    track_factory: Callable[[int, int], Dict[str, str]],
+):
+    habit = habit_factory(int(test_user["id"]), "Pushups")
+
+    track_factory(int(test_user["id"]), int(habit["id"]))
+    track_factory(int(test_user["id"]), int(habit["id"]))
+
+    response = client.get(f"/track?user_id={test_user['id']}&habit_id={habit['id']}")
+    assert response.status_code == 200
+
+    tracks = response.json()
+    assert all(t["habit_id"] == habit["id"] for t in tracks)
+    assert len(tracks) >= 2
